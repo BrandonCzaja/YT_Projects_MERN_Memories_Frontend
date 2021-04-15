603779a714804512e6587bcf60003786995b74b1
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-04-14 19:47:14 -0500

    Added the reducer to the index file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
+// Importing the reducer
 import posts from "./posts";
 
 export default combineReducers({
-	posts,
+	// Since the key and value are the same, I could just write a single 'posts'
+	posts: posts,
 });
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -2,16 +2,18 @@
 // I am passing posts, but for this posts is actually state
 // In reducers, state must always have a value, which is why we set the initial value as an empty array
 
-const reducer = (posts = [], action) => {
+export default (posts = [], action) => {
 	if (action.type === "CREATE") {
 		//   return action || return state changed by the action
 
 		// A lot of things can happen in reducers, so it is best to have a switch statement
 		switch (action.type) {
 			case "FETCH_ALL":
+				// We will return logic, but for now we will just return state
 				return posts;
 				break;
 			case "CREATE":
+				// We will return logic, but for now we will just return state
 				return posts;
 				break;
 			default:
