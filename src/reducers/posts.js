158fd6ead89b728b17dc20f04bf3b743be36d66b
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-04-14 20:10:10 -0500

    created FETCH_ALL logic with notes for the reducer

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -9,8 +9,8 @@ export default (posts = [], action) => {
 		// A lot of things can happen in reducers, so it is best to have a switch statement
 		switch (action.type) {
 			case "FETCH_ALL":
-				// We will return logic, but for now we will just return state
-				return posts;
+				// As soon as the action gets dispatched it comes here. Since we are immediately fetching the posts from the action we can just return the action payload, which is the actual posts
+				return action.payload;
 				break;
 			case "CREATE":
 				// We will return logic, but for now we will just return state
