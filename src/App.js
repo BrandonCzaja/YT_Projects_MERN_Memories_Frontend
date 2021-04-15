a2d9e83cd4d49b24cc2a059d991322495b26a0a2
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-04-14 19:55:12 -0500

    Created a dispatch that calls the getPosts action inside of useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react"; // useEffect for the dispatch
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux"; // Dispatch an action
+
+import { getPosts } from "./actions/posts";
 import memories from "./images/memories.png";
 import Posts from "./components/posts/posts";
 import Form from "./components/form/form";
@@ -7,6 +10,12 @@ import useStyles from "./styles";
 
 const App = () => {
 	const classes = useStyles();
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(getPosts());
+	}, []);
+
 	return (
 		<Container maxwidth="lg">
 			<AppBar className={classes.appBar} position="static" color="inherit">
