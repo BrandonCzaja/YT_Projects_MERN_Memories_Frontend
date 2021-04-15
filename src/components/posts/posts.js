2f03efa172e10fcd7efa4b3b69d5e6e827a02694
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-04-14 20:36:26 -0500

    Fixed bug in the reducers posts.js file

--- src/components/posts/posts.js
+++ src/components/posts/posts.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Post from "./post/post";
 import useStyles from "./styles";
+import { useSelector } from "react-redux"; // Fetch the data from the global redux store
 
 const Posts = () => {
+	const posts = useSelector((state) => state.posts); // It is called posts from the reducers => index.js file
 	const classes = useStyles();
+
+	console.log(posts);
+
 	return (
 		<>
 			<h1>Posts</h1>
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -3,21 +3,19 @@
 // In reducers, state must always have a value, which is why we set the initial value as an empty array
 
 export default (posts = [], action) => {
-	if (action.type === "CREATE") {
-		//   return action || return state changed by the action
+	//   return action || return state changed by the action
 
-		// A lot of things can happen in reducers, so it is best to have a switch statement
-		switch (action.type) {
-			case "FETCH_ALL":
-				// As soon as the action gets dispatched it comes here. Since we are immediately fetching the posts from the action we can just return the action payload, which is the actual posts
-				return action.payload;
-				break;
-			case "CREATE":
-				// We will return logic, but for now we will just return state
-				return posts;
-				break;
-			default:
-				return posts;
-		}
+	// A lot of things can happen in reducers, so it is best to have a switch statement
+	switch (action.type) {
+		case "FETCH_ALL":
+			// As soon as the action gets dispatched it comes here. Since we are immediately fetching the posts from the action we can just return the action payload, which is the actual posts
+			return action.payload;
+			break;
+		case "CREATE":
+			// We will return logic, but for now we will just return state
+			return posts;
+			break;
+		default:
+			return posts;
 	}
 };
